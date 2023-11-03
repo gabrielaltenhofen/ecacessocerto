93c884aeb48939fded5d8bcfbfa5fc2981cb0b03
gabrielaltenhofen <ga0125419@setrem.com.br>
2023-11-03 10:36:15 -0300

    ajuste permissões

--- js/auth-guard.js
+++ js/auth-guard.js
@@ -1,8 +1,35 @@
 firebase.auth().onAuthStateChanged(user => {
-    if (!user) {
-        window.location.href = "index.html";
+    if (user) {
+        const userEmail = user.email;
+        
+        isUserAdmin(userEmail).then(isAdmin => {
+            if (isAdmin) {
+                // Usuário é um administrador, permita o acesso a páginas de admin
+                if (window.location.pathname === "/menu.html" || 
+                    window.location.pathname === "/cadastro.html" ||
+                    window.location.pathname === "/users.html" ||
+                    window.location.pathname === "/historico.html" ||
+                    window.location.pathname === "/gerarcodigo.html") {
+                    // O usuário pode acessar esta página
+                } else {
+                    // Redirecione para uma página de acesso negado
+                    redirectToLoginPage();
+                }
+            } else {
+                if (window.location.pathname === "/home.html" || window.location.pathname === "/codigouser.html") {
+                    // O usuário pode acessar estas páginas
+                } else {
+                    // Redirecione para uma página de acesso negado
+                    redirectToLoginPage();
+                }
+            }
+        });
+    } else {
+        // O usuário não está autenticado, redirecione para a página de login
+        redirectToLoginPage();
     }
-})
+});
+
 
 function getCookie(name) {
     const cookies = document.cookie.split(';');
@@ -13,4 +40,24 @@ function getCookie(name) {
         }
     }
     return null;
+}
+
+// Função para verificar se o usuário é um administrador
+function isUserAdmin(userEmail) {
+    const usersRef = firebase.database().ref('usuarios');
+    return usersRef.orderByChild('email').equalTo(userEmail).once('value').then(snapshot => {
+        if (snapshot.exists()) {
+            const userCPF = Object.keys(snapshot.val())[0];
+            if (userCPF) {
+                const userData = snapshot.val()[userCPF];
+                return userData.nivelAcesso === 'admin';
+            }
+        }
+        return false;
+    });
+}
+
+// Função para redirecionar o usuário para a página de login se não estiver autenticado
+function redirectToLoginPage() {
+    window.location.href = "index.html";
 }
